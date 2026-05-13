43927352a2b5fbc333fe154bdd6f7bf83b9c882f
root <root@srv1581978.hstgr.cloud>
2026-05-13 20:47:28 +0000

    fix: remove registerSW + splash screens iOS do index.html (Safari Mac tela branca)

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,5 @@
 import { createRoot } from "react-dom/client";
-import { registerSW } from "virtual:pwa-register";
 import App from "./App.tsx";
 import "./index.css";
-
-const updateSW = registerSW({
-  immediate: true,
-  onNeedRefresh() {
-    // Quando detecta que tem código novo no servidor, recarrega a página sozinho
-    updateSW(true);
-  }
-});
 
 createRoot(document.getElementById("root")!).render(<App />);
